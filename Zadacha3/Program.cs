ddc7620c08bae398d5ceb63de5ce888d5b8ee3a9
SkyLearn <ba_de@mail.ru>
2023-01-22 19:00:45 +0300

    Готово

--- Zadacha3/Program.cs
+++ Zadacha3/Program.cs
@@ -5,31 +5,30 @@ Console.Write("Массив: ");
 
 double[] array = new double[5];
 double result = 0;
-double max = array[0];
-double min = array[0];
+
 Random random_number = new Random();
 
 
 for (int i = 0; i < array.Length; i++)
 {
-    array[i] = Math.Round((random_number.NextDouble() * random_number.Next(-100,100)), 1);
+    array[i] = Math.Round((random_number.NextDouble() * random_number.Next(0,100)), 1);
     Console.Write(array[i] + " ");
 }
+double max = array[0];
+double min = array[0];
 
 for (int i = 0; i < array.Length; i++)
 {
     if (max < array[i])
     {
         max = array[i];
+    
     }
-
-    else
-
-    if (min > array[i])
+       if (min > array[i])
     {
         min = array[i];
+    
     }
-
 }
 
 result = (max - min);
